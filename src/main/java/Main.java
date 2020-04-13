9267eb0b8ca59d1a44e75433dd3e68a1356ff484
shutovai <Pentalgin09@mail.ru>
2020-04-13 21:39:45 +0300

    Commit 8

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,8 @@
+import org.apache.log4j.BasicConfigurator;
+
 public class Main {
     public static void main(String[] args) {
-
+        BasicConfigurator.configure();
         MyServer server = new MyServer();
         server.start();
     }
--- src/main/java/UserEntity.java
+++ src/main/java/UserEntity.java
@@ -1,26 +1,24 @@
 import lombok.SneakyThrows;
+import org.apache.log4j.Logger;
 
 import java.sql.*;
 import java.io.*;
 import java.net.Socket;
-import java.util.ArrayList;
-
 
 public class UserEntity implements Runnable, Observer {
     private User user;
     private Socket socket;
     private MyServer server;
+    private static final Logger log = Logger.getLogger(UserEntity.class);
 
     public UserEntity(Socket socket, MyServer server) {
         this.socket = socket;
         this.server = server;
     }
 
-    //    ArrayList<String> chat = new ArrayList();
     @SneakyThrows
     @Override
     public void run() {
-
         PrintWriter clientWriter = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
         BufferedReader clientReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
         while (true) {
@@ -29,14 +27,14 @@ public class UserEntity implements Runnable, Observer {
                 String[] logPass = clientMessage.split(":");
                 String s = logPass[1].replaceAll("REGISTRATION", "");
                 logPass[1] = s;
-                String checkLogin = null;
+                log.info("Запрос дошел до проверки пользователя в БД!");
                 try (Connection conn = DriverManager.getConnection("jdbc:MySQL://localhost:3306/my_schema?serverTimezone=UTC",
                         "root", "123456")) {
+                    log.info("Подключение к БД прошло упешно!");
                     ResultSet resultSet = conn.prepareStatement("SELECT login from users").executeQuery();
                     boolean flag = false;
                     while (resultSet.next()) {
-                        checkLogin = checkLogin + resultSet.getString("login");
-                        if(checkLogin.contains(logPass[0])){
+                        if (logPass[0].equalsIgnoreCase(resultSet.getString("login"))) {
                             flag = true;
                             break;
                         }
@@ -45,6 +43,8 @@ public class UserEntity implements Runnable, Observer {
                         PrintWriter writer = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
                         writer.println("Данный пользователь уже зарегистрирован!");
                         writer.flush();
+                        log.info("При попытке регистрации, произошла ошибка! Введенный пользователь уже зарегистрирован!");
+                        log.error("При попытке регистрации, произошла ошибка! Введенный пользователь уже зарегистрирован!");
                     } else {
                         PrintWriter writer2 = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
                         writer2.println("Регистрация прошла успешно!");
@@ -52,10 +52,14 @@ public class UserEntity implements Runnable, Observer {
                         user = new User(logPass[0], logPass[1]);
                         System.out.println("Подключен новый пользователь: " + logPass[0]);
                         server.addObserver(this);
+                        log.info("Регистрация прошла успешно! Новый пользователь подключен!");
                         PreparedStatement preparedStatement = conn.prepareStatement("INSERT INTO users(login, password) values(?, ?)");
                         preparedStatement.setString(1, user.getLogin());
                         preparedStatement.setString(2, user.getPassword());
                         preparedStatement.executeUpdate();
+                        log.info("Добавление пользователя в БД");
+                        log.error("Ошибка при добавлении пользователя в БД!");
+
                     }
                 } catch (SQLException e) {
                     e.printStackTrace();
@@ -73,17 +77,17 @@ public class UserEntity implements Runnable, Observer {
                     boolean flag = false;
                     while (resultSet.next()) {
                         res2 = res2 + resultSet.getString("login") + resultSet.getString("password");
-                        if(res.equalsIgnoreCase(resultSet.getString("login") + resultSet.getString("password"))){
+                        if (res.equalsIgnoreCase(resultSet.getString("login") + resultSet.getString("password"))) {
                             flag = true;
                             break;
                         }
                     }
-                    if(flag){
+                    if (flag) {
                         PrintWriter clientWriter2 = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
                         clientWriter2.println("Вход успешно выполнен!");
                         clientWriter2.flush();
                         user = new User(logPass[0], logPass[1]);
-                        System.out.println("New user connected: " + logPass[0]);
+                        System.out.println("Подключен пользователь: " + logPass[0]);
                         server.addObserver(this);
                     } else {
                         PrintWriter clientWriter3 = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
@@ -93,11 +97,12 @@ public class UserEntity implements Runnable, Observer {
                 } catch (SQLException e) {
                     e.printStackTrace();
                 }
-//                break;
-//                }
+
+            } else if (clientMessage.startsWith("сообщение для " )) {
+
 
             } else if (clientMessage.startsWith("exit")) {
-                System.out.println("Пользователь: " + user.getLogin() + " disconnected");
+                System.out.println("Пользователь: " + user.getLogin() + " покинул чат!");
                 clientWriter.println(user.getLogin() + " " + "Покинул чат!");
                 server.stopObserver(this);
                 clientWriter.flush();
@@ -108,10 +113,10 @@ public class UserEntity implements Runnable, Observer {
                 FileWriter fileWriter = new FileWriter("chat.txt", true);
                 fileWriter.write(user.getLogin() + ": " + clientMessage + "\n");
                 fileWriter.flush();
+
             }
         }
     }
-
     @SneakyThrows
     @Override
     public void notifyObserver(String message) {
